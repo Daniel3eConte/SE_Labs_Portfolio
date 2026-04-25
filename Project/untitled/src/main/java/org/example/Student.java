1d489c7daa74c74c75262f5b353c96045c9c0295
calliopekyd <905841@edu.p.lodz.pl>
2026-04-25 12:04:36 +0200

    solution task 2

--- Project/untitled/src/main/java/org/example/Student.java
+++ Project/untitled/src/main/java/org/example/Student.java
@@ -1,11 +1,13 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
     private String studentID;
     private String name;
     private String email;
     private int credits;
-
+    private List<Activity> activities;
     public Student(String studentID, String name, String email, int credits) {
         this.studentID = studentID;
         this.name = name;
@@ -14,7 +16,11 @@ public class Student {
     }
 
     public Activity createActivity(String activityName, int price, String description) {
-        return new Activity(activityName, price, description);
+        Activity activity = new Activity(activityName, price, description);
+        activities.add(activity);
+        return activity;    }
+    public List<Activity> getActivities() {
+        return activities;
     }
 
 }
