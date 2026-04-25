ec36d54f546e48f69b31232c1994380970d7d263
Spirti <cdanielescuola@gmail.com>
2026-04-25 12:25:05 +0200

    task 4 activities list

--- Project/untitled/src/test/java/org/example/StudentTest.java
+++ Project/untitled/src/test/java/org/example/StudentTest.java
@@ -40,4 +40,19 @@ class StudentTest {
         assertEquals(1, student.getActivities().size());
         assertTrue(student.getActivities().contains(activity));
     }
+    @Test
+    void createdActivityIsAddedToCategory() {
+        Student student = new Student("S001", "Anna", "anna@edu.p.lodz.pl", 10);
+        Category category = new Category("Tutoring");
+
+        Activity activity = student.createActivity(
+                "Java tutoring",
+                5,
+                "I can help with Java basics",
+                category
+        );
+
+        assertEquals(1, category.getActivities().size());
+        assertTrue(category.getActivities().contains(activity));
+    }
 }
