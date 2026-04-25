027b0881e2281a4537c717386ea4a93fa7d47b1a
calliopekyd <905841@edu.p.lodz.pl>
2026-04-25 12:18:19 +0200

    task 3 category

--- Project/untitled/src/test/java/org/example/StudentTest.java
+++ Project/untitled/src/test/java/org/example/StudentTest.java
@@ -3,34 +3,38 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StudentTest {
 
     @Test
-    void studentCanCreateActivity() {
+    void studentCanCreateActivityWithCategory() {
         Student student = new Student("S001", "Anna", "anna@edu.p.lodz.pl", 10);
+        Category category = new Category("Tutoring");
 
         Activity activity = student.createActivity(
                 "Java tutoring",
                 5,
-                "I can help with Java basics"
+                "I can help with Java basics",
+                category
         );
 
         assertEquals("Java tutoring", activity.getActivityName());
         assertEquals(5, activity.getPrice());
         assertEquals("I can help with Java basics", activity.getDescription());
+        assertEquals(category, activity.getCategory());
     }
 
     @Test
     void createdActivityIsSavedInsideStudentActivities() {
         Student student = new Student("S001", "Anna", "anna@edu.p.lodz.pl", 10);
+        Category category = new Category("Tutoring");
 
         Activity activity = student.createActivity(
                 "Java tutoring",
                 5,
-                "I can help with Java basics"
+                "I can help with Java basics",
+                category
         );
 
         assertEquals(1, student.getActivities().size());
